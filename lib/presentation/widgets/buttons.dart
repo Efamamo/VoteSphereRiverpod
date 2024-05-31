4a0b8ef97b94797ed5d8897df6157a16b079ac0c
yohannesalex <yohannesalemayehu624@gmail.com>
2024-05-31 23:04:51 +0300

    feat: add lib/presentation/widgets/buttons.dart

---
+++ lib/presentation/widgets/buttons.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  const MyButton(
+      {super.key,
+      required this.color,
+      required this.child,
+      required this.onTapped});
+  final Widget child;
+  final void Function(BuildContext) onTapped;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+        backgroundColor: Colors.blue[700],
+        foregroundColor: Colors.white,
+      ),
+      child: const Text('Create group'),
+      onPressed: () {
+        onTapped;
+      },
+    );
+  }
+}
