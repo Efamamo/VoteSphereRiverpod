9195bc66232dfecf43e0f54d3b3a51e2cbdf2070
yohannesalex <yohannesalemayehu624@gmail.com>
2024-05-31 23:04:46 +0300

    feat: add lib/infrastructure/repositories/auth_repository.dart

---
+++ lib/infrastructure/repositories/auth_repository.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../data_provider/auth_dataprovider.dart';
+import '../local_storage/secure_storage.dart';
+
+class AuthRepository {
+  static Future<String> login(String username, String password) async {
+    Map response = await AuthDataProvider.login(username, password);
+
+    if (response.containsKey('message')) {
+      var error = response["message"];
+      if (error is! String) {
+        error = error[0];
+      }
+      return error;
+    } else {
+      final secureStorage = SecureStorage().secureStorage;
+      await secureStorage.write(key: "role", value: response["role"]);
+      await secureStorage.write(key: "token", value: response["accessToken"]);
+      await secureStorage.write(key: "username", value: response["username"]);
+      await secureStorage.write(key: "group", value: response["groupID"]);
+      await secureStorage.write(key: "email", value: response["email"]);
+      return "success";
+    }
+  }
+
+  static Future<String> signUp(
+      String username, String password, String email, String role) async {
+    Map response =
+        await AuthDataProvider.signUp(username, password, email, role);
+
+    if (response.containsKey('message')) {
+      var error = response["message"];
+      if (error is! String) {
+        error = error[0];
+      }
+      return error;
+    } else {
+      final secureStorage = SecureStorage().secureStorage;
+      await secureStorage.write(key: "role", value: response["role"]);
+      await secureStorage.write(key: "token", value: response["accessToken"]);
+      await secureStorage.write(key: "username", value: response["username"]);
+      await secureStorage.write(key: "group", value: response["groupID"]);
+      await secureStorage.write(key: "email", value: response["email"]);
+      return "success";
+    }
+  }
+
+  static Future<void> signout() async {
+    final secureStorage = SecureStorage().secureStorage;
+    await secureStorage.deleteAll();
+  }
+}
